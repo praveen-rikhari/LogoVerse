d134541bdc0e4733fbbda05569e35cc85cfeac63
praveen-rikhari <praveenrikhari45@gmail.com>
2025-02-16 16:43:36 +0530

    done

--- app/create/_components/LogoTitle.jsx
+++ app/create/_components/LogoTitle.jsx
@@ -1,13 +1,10 @@
 "use client"
 
-import React, { useState } from 'react';
+import React from 'react';
 import HeadingDescription from './HeadingDescription';
 import Lookup from '../../_data/Lookup';
-import { useSearchParams } from 'next/navigation';
 
 function LogoTitle({onHandleInputChange}) {
-    const params = useSearchParams();
-    const [title , setTitle] = useState(params?.get('title') ?? '');
 
     return (
         <div className='my-6'>
